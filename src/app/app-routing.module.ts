29c8d92ac4471c8d532a87c92fbb6069ac9aa2de
Jader <swilor29@gmail.com>
2021-01-07 11:15:34 -0500

    maqueta navbar lista con responsive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'home', loadChildren: () => import('./home/pages/home/home.module').then(m => m.HomeModule) 
+  },
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'home'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
